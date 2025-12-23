f5ca1dc4f0d4484611b4c2ab311dd207088a4790
popugai4iks.maximka@mail.ru <popugai4iks.maximka@mail.ru>
2025-12-23 17:18:35 +0300

    feat: add schemas

---
+++ app/schemas/tweet.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from typing import List
+
+class TweetCreate(BaseModel):
+    tweet_data: str
+    tweet_media_ids: List[int] | None = None
+
+class LikeUser(BaseModel):
+    user_id: int
+    name: str
+
+class Author(BaseModel):
+    id: int
+    name: str
+
+class TweetInFeed(BaseModel):
+    id: int
+    content: str
+    attachments: List[str]
+    author: Author
+    likes: List[LikeUser]
+
+class TweetsFeedResponse(BaseModel):
+    result: bool
+    tweets: List[TweetInFeed]
---
+++ app/schemas/user.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from typing import List
+
+class UserBase(BaseModel):
+    name: str
+
+class User(UserBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+class ProfileUser(User):
+    followers: List[User] = []
+    following: List[User] = []
+
+class ProfileResponse(BaseModel):
+    result: bool
+    user: ProfileUser
